15d83d222f9c62a08d28c634f6ad8c7db14527d5
Theo2801 <theologos.bourloglou@gmail.com>
2024-03-02 00:56:07 +0200

    [feat] Design Header, Hero, Add Fonts, Media

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily : {
+      proxima:        ['"proxima-nova", sans-serif'],
+      unineueRegular: ['"Unineue Regular", sans-serif'],
+      unineueBold:    ['"Unineue Bold", sans-serif']
+    },
+    extend: {
+      colors: {
+        'gray-1'  : '#232323',
+        'gray-2'  : '#414141',
+        'gray-3'  : '#d3d3d3',
+        'gray-4'  : '#e4e4e4',
+        'primary' : '#0883ba',
+      },
+    },
   },
   plugins: [],
 }

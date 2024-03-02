84fe3ddb120e8b2ced23a65c4a0e8ff7721ae2c3
Theo2801 <theologos.bourloglou@gmail.com>
2024-03-03 01:58:41 +0200

    [feat] Add rest of components and media

--- app.js
+++ app.js
@@ -14,6 +14,48 @@ const fetchComponents = (components) => {
           if (element) {
             element.innerHTML = data;
           }
+          loadBoxShadows();
         });
     });
   };
+
+/**
+ * Create and associate box shadows in elements.
+ *
+ * @returns {void}
+ */
+const loadBoxShadows = () => {
+    let shadow = '';
+    for (let i = 0; i < 400; i++) {
+        shadow += (shadow? ',':'')+ -i*2+'px ' + i*1+'px 0 #d3d3d3';
+    }
+    document.querySelectorAll(".service-item").forEach(item => {
+        item.style.boxShadow = shadow;
+    });
+}
+
+/**
+ * Fetches all the products from the JSON file.
+ *
+ * @returns {void}
+ */
+const fetchData = () => {
+    return {
+        data: {},
+        loadData() {
+            fetch('data.json')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(json => {
+                    this.data = json;
+                })
+                .catch(error => {
+                    console.error('There has been a problem with your fetch operation:', error);
+                });
+        }
+    };
+  }

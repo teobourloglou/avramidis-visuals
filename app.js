1bcf684e72e468c8fcf900753ceaac171c92c96b
Theo2801 <theologos.bourloglou@gmail.com>
2024-03-03 16:38:21 +0200

    [feat] Add animations

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+window.addEventListener('scroll', function() {
+    let scrollPosition = window.scrollY;
+    let triggerPosition = 300; // Adjust this to your desired scroll position
+    let desktopLinks = document.querySelectorAll('.desktopLinks');
+
+    if (scrollPosition >= triggerPosition) {
+        document.getElementById('headerDesktop').classList.add('bg-gray-4');
+        document.getElementById('contactButton').classList.add('bg-gray-1', 'text-gray-4');
+        document.getElementById('contactButton').classList.remove('bg-gray-4', 'text-gray-1');
+        document.getElementById('headerDesktopLogo').src = 'media/black-logo.svg';
+        document.getElementById('openMenuButton').classList.add('text-gray-1');
+        document.getElementById('openMenuButton').classList.remove('text-gray-4');
+        desktopLinks.forEach(function(element) {
+            element.classList.add('text-gray-1');
+            element.classList.remove('text-gray-4');
+        });
+    } else {
+        document.getElementById('headerDesktop').classList.remove('bg-gray-4');
+        document.getElementById('contactButton').classList.remove('bg-gray-1', 'text-gray-4');
+        document.getElementById('contactButton').classList.add('bg-gray-4', 'text-gray-1');
+        document.getElementById('headerDesktopLogo').src = 'media/white-logo.svg';
+        document.getElementById('openMenuButton').classList.remove('text-gray-1');
+        document.getElementById('openMenuButton').classList.add('text-gray-4');
+        desktopLinks.forEach(function(element) {
+            element.classList.remove('text-gray-1');
+            element.classList.add('text-gray-4');
+        });
+    }
+});
+
 /**
  * Fetch and load components into specified element IDs.
  *
@@ -42,6 +72,7 @@ const loadBoxShadows = () => {
 const fetchData = () => {
     return {
         data: {},
+        scroll: false,
         loadData() {
             fetch('data.json')
                 .then(response => {
